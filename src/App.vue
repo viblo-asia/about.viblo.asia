928ca719637b82b1c870a1e1da3a3e728240799e
thangbtx-0846 <47617090+thangbtx-0846@users.noreply.github.com>
2020-02-17 14:38:12 +0700

    chore: setup vue project (#1)

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="app">
+    <h1 class="text-xl text-gray-900 text-center">Hello world!!!</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style lang="scss">
+@tailwind base;
+@tailwind components;
+@tailwind utilities;
+</style>
