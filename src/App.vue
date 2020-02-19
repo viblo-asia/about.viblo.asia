950de900c83569b1d2cf62bc6633b451898aa2ab
thangbtx-0846 <47617090+thangbtx-0846@users.noreply.github.com>
2020-02-19 10:17:39 +0700

    feat: add first section & Intro section (#2)
    Redmine: https://dev.sun-asterisk.com/issues/439953

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,20 @@
 <template>
   <div id="app">
-    <h1 class="text-xl text-gray-900 text-center">Hello world!!!</h1>
+    <FirstSection />
+    <IntroSection />
   </div>
 </template>
 
 <script>
+import FirstSection from './components/FirstSection'
+import IntroSection from './components/IntroSection'
+
 export default {
-  name: 'App'
+  name: 'App',
+  components: {
+    FirstSection,
+    IntroSection
+  }
 }
 </script>
 
@@ -14,4 +22,13 @@ export default {
 @tailwind base;
 @tailwind components;
 @tailwind utilities;
+
+body {
+  background: #01001e;
+  font-family: 'Roboto',-apple-system,BlinkMacSystemFont,Segoe UI,Helvetica Neue,Arial,sans-serif,Apple Color Emoji,Segoe UI Emoji,Segoe UI Symbol;
+}
+
+.blend-screen {
+  mix-blend-mode: screen;
+}
 </style>
---
+++ src/components/IntroSection.vue
@@ -0,0 +1,25 @@
+<template>
+  <section id="intro-section" class="flex items-center justify-center bg-white">
+    <div id="intro-text" class="max-w-lg md:text-lg text-md text-blue-600 lg:pt-32 pt-16 lg:pb-32 pb-16 pl-5 pr-5">
+      <p><strong>Viblo</strong> (https://viblo.asia) là cộng đồng chia sẻ kiến thức về Công nghệ thông tin.</p>
+
+      <p>Bạn có thể viết và chia sẻ nội dung liên quan đến các vấn đề
+        kỹ thuật như: <strong>Development</strong> (chủ đề lập trình, infrastructure...),
+        <strong>Design</strong> (thiết kế trong quá trình sản xuất phần mềm), <strong>QA</strong> (test,
+        đảm bảo chất lượng dự án...), <strong>Management</strong> (quản lý dự án).</p>
+
+      <p>Hiện tại Viblo hỗ trợ viết bài bằng ngôn ngữ Tiếng Việt, Tiếng
+        Anh và Tiếng Nhật.</p>
+    </div>
+  </section>
+</template>
+
+<style lang="scss" scoped>
+  #intro-section {
+    #intro-text {
+      & > * + *,  & li + li, & li > p + p {
+        @apply mt-6;
+      }
+    }
+  }
+</style>
