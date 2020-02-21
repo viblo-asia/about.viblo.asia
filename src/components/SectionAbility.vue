102d11cfbdf02b638c98c3ea9243e891f26ba64c
Pham Van Hung <61034830+hungpv-2151@users.noreply.github.com>
2020-02-21 10:32:50 +0700

    feat: add milestone section (#4)
    * add image, add mockup section, basicly complete feature and milestone
    * change the css for line time and feature
    * test git ssh
    
    https://dev.sun-asterisk.com/issues/443220
    https://dev.sun-asterisk.com/issues/443214

---
+++ src/components/SectionAbility.vue
@@ -0,0 +1,134 @@
+<template>
+    <section id="section-ability" class="text-center py-16 px-16">
+        <div class="section-head text-center">
+            <h3 class="uppercase text-4xl">Chức năng <span class="font-black">chủ đạo</span></h3>
+        </div>
+        <div class="section-content flex flex-wrap justify-between px-8 md:px-16">
+            <div class="content-item text-left mb-8 w-full">
+                <h4 class="content-title uppercase flex items-center text-white">
+                    <span class="content-icon p-1 mr-2 inline-block bg-white border-4 border-solid rounded-full">
+                        <svg class="h-3" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path fill="#FFF" d="M15 3l-4-2v1L6 8l-2 3-1 1v6l1 1 5-2 1-1 2-3 5-7c0-1 0-2-2-3zM9 15l-2 1-1-1-2-1v-2l1-1 3 1 2 3H9z"/></svg>
+                    </span>
+                    viết bài
+                </h4>z
+                <div class="content-text my-3">
+                    <p>Để viết bài, bạn c hỉ cầ n nhấp chu ộ t vào nút "Crest Post" trên trang chủ Viblo hoặc b iểu tượn g cây bút đầu trang. Nhập tiêu đề, gắn fag, viết nội dung vào các mục fương ứng và đừng quên lựa chọn ngôn ngữ</p>
+                </div>
+                <div class="content-image flex flex-wrap justify-between">
+                    <img src="../assets/write post.svg" alt="viet bai image" />
+                </div>
+            </div>
+            <div class="content-item text-left mb-8 w-full">
+                <h4 class="content-title uppercase flex items-center text-white">
+                    <span class="content-icon p-1 mr-2 inline-block bg-white border-4 border-solid rounded-full">
+                        <svg class="h-3" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path fill="#FFF" d="M14 2l-4-1-4 1-2 4h4V5l2-1h2v3l-1 1-2 2-1 3h3v-1l1-1 1-1 2-2 1-2-2-4zm-4 13l-2 2c0 1 0 2 2 2l2-2-2-2z"/></svg>
+                    </span>
+                    Đặt câu hỏi & trả câu hỏi
+                </h4>
+                <div class="content-text my-3">
+                    <p>Là một trong những tính năng chủ đạo của Viblo, đây là nỏi bạn có thể đặt câu hỏi cũng như tham gia trả lời câu hỏi của người dùng khác.</p>
+                </div>
+                <div class="content-image flex flex-wrap justify-between">
+                    <img src="../assets/quest n answer.svg" alt="viet bai image" />
+                </div>
+            </div>
+            <div class="content-item text-left mb-8 w-full">
+                <h4 class="content-title uppercase flex items-center text-white">
+                    <span class="content-icon p-1 mr-2 inline-block bg-white border-4 border-solid rounded-full">
+                        <svg class="h-3" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path fill="#FFF" d="M8 2L6 5l1 2H6l1 2 1 3-3 3c-2 1-4 2-4 4v1h18v-1c0-2-2-3-4-4l-3-3 1-3 1-2h-1l1-2c0-1-1-3-3-3l1-2C9 0 9 1 8 2z"/></svg>
+                    </span>
+                    Follow users
+                </h4>
+                <div class="content-text my-3">
+                    <p>Theo dõi một người dùng cụ thể mà bạn yêu thích để cập nhập những bài viết mới nhất từ họ, đặc biệt là những thành viên nổi bật.</p>
+                </div>
+                <div class="content-image flex flex-wrap md:flex-no-wrap justify-between">
+                    <span class="w-1/4"><img src="../assets/follow.png" alt="viet bai image" class="w-full" /></span>
+                    <span class="w-2/3"><img src="../assets/follow 1.png" alt="viet bai image" class="h-full" /></span>
+                </div>
+            </div>
+            <div class="content-item text-left mb-8 w-1/3">
+                <h4 class="content-title uppercase flex items-center text-white">
+                    <span class="content-icon p-1 mr-2 inline-block bg-white border-4 border-solid rounded-full">
+                        <svg class="h-3" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path fill="#FFF" d="M19 6L5 19l1-5-5 1L14 1h2l3 3v2z"/></svg>
+                    </span>
+                    tags
+                </h4>
+                <div class="content-text my-3">
+                    <p>Danh sách Popular tags được đặt bên phải dưới các trang, gips bạn lựa chọn và theo dõi chủ đề mình quan tâm dễ dàng</p>
+                </div>
+                <div class="content-image flex flex-wrap justify-between">
+                    <img src="../assets/tag.svg" alt="viet bai image" />
+                </div>
+            </div>
+            <div class="content-item text-left mb-8 w-3/5">
+                <h4 class="content-title uppercase flex items-center text-white">
+                    <span class="content-icon p-1 mr-2 inline-block bg-white border-4 border-solid rounded-full">
+                        <svg class="h-3" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path fill="#FFF" d="M15 10v1l-9 5-1-1V5l1-1 9 5v1z"/></svg>
+                    </span>
+                    techical videos
+                </h4>
+                <div class="content-text my-3">
+                    <p>Viblo Team có tiến hành ghi hình nhiều bài thuyết trình ở những sự kiện công nghệ nổi tiếng, sau khi được biên tập, các video sẽ được chia sẻ trên Viblo.</p>
+                </div>
+                <div class="content-image flex flex-wrap justify-between">
+                    <img src="../assets/video.svg" alt="viet bai image" />
+                </div>
+            </div>
+            <div class="content-item text-left mb-8 w-full">
+                <div class="flex">
+                    <div class="w-1/2 pr-4">
+                        <h4 class="content-title uppercase flex items-center text-white">
+                            <span class="content-icon p-1 mr-2 inline-block bg-white border-4 border-solid rounded-full">
+                                <svg class="h-3" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path fill="#FFF" d="M14 7s3-3 1-6l-5 5c-1 1-4 2-4 4v6c0 2 5 3 9 3l3-10-4-2zM5 7c-1 0-3 1-3 4v4c0 3 2 3 3 3l-1-2v-6l1-3z"/></svg>
+                            </span>
+                            rate contents
+                        </h4>
+                        <div class="content-text my-3">
+                            <p>Chấm điểm cho bài viết, câu hỏi, câu trả lời, comment bằng cách nhấn vote up hoặc vote down ở phía trên bên trái của bài viết đó.</p>
+                        </div>
+                    </div>
+                    <div class="w-1/2 pl-4">
+                        <h4 class="content-title uppercase flex items-center text-white">
+                            <span class="content-icon p-1 mr-2 inline-block bg-white border-4 border-solid rounded-full">
+                                <svg class="h-3" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path fill="#FFF" d="M6 20l-4-2c-1-1-2-4 0-7L12 1l4-1 2 3-1 3-9 10-2 1-2-1 1-3 6-7h1v1l-6 7-1 1h2l9-10 1-2-2-1h-2L3 12v5c1 2 3 2 5 0L18 7h1v1L9 18l-3 2z"/></svg>
+                            </span>
+                            clip pots
+                        </h4>
+                        <div class="content-text my-3">
+                            <p>Clip một bài viết để xem lại khi cần thiết, bằng cách nhấn vào ô <strong>"Clip this post"</strong> phía trên bên phải của bài viết.</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="content-image flex flex-wrap justify-between">
+                    <img src="../assets/rate.svg" alt="viet bai image" />
+                </div>
+            </div>
+            <div class="content-item text-left mb-8 w-full">
+            </div>
+        </div>
+    </section>
+</template>
+
+<style lang="scss">
+#section-ability {
+    .content-title {
+        .content-icon {
+            border-color: #1B13DB;
+            svg {
+                path {fill: #000}
+            }
+        }
+    }
+    .content-text {text-align: justify}
+    .content-image {
+        img {border: 10px solid rgba(255,255,255,.1)}
+        span {
+            max-height: 160px;
+            overflow: hidden;
+            border: 10px solid rgba(255,255,255,.1);
+            img {border: none}
+        }
+    }
+}
+</style>
---
+++ src/components/SectionMilestone.vue
@@ -0,0 +1,157 @@
+<template>
+    <section id="section-milstone" class="text-center py-32 px-16">
+        <div class="section-head text-center">
+            <h3 class="uppercase text-4xl">Viblo <span class="font-black">milestone</span></h3>
+        </div>
+        <div class="section-content px-8 md:px-8">
+            <div class="content-item">
+                <div class="item-title">10.2014</div>
+                <div class="item-description">Dự án Viblo (Vietnam IT Blog) đưỢC khởi động với mụ c đích tạo ra một cộng đồng nơi thành viên có thể chia sẻ , họ c hỏi kiến thức Công nghệ | thông tin bằng tiếng mẹ đẻ.</div>
+            </div>
+            <div class="content-item">
+                <div class="item-title">03.11.2014</div>
+                <div class="item-description">Commit đầu tiên của dự án Viblo được thực hiện với nội dun g "First commit: Add Laravel 4.2"</div>
+            </div>
+            <div class="content-item">
+                <div class="item-title">03.04.2015</div>
+                <div class="item-description">Viblo chính thức được ra mắt.</div>
+            </div>
+            <div class="content-item">
+                <div class="item-title">10.2016</div>
+                <div class="item-description">Viblo được tái t ạo , sử dụn g công nghệ, n gôn ngữ thiết kế mới hơn, hiện đại hơn, phục vụ tốt hơn cho quá trình mở rộng.</div>
+            </div>
+            <div class="content-item">
+                <div class="item-title">29.12.2016</div>
+                <div class="item-description">Viblo Version 2 được ra mắt, thay thế hoàn toàn phiên bản cũ với logo, design được làm mới, và rất nhiều tính năng bổ sung.</div>
+            </div>
+            <div class="content-item">
+                <div class="item-title">24.03.2017</div>
+                <div class="item-description">Ra mắt Chrome Extension, giúp người dùn g dễ d àng tiếp cậ n nội dung, thông báo mới từ phía Viblo</div>
+            </div>
+            <div class="content-item">
+                <div class="item-title">15.05.2017</div>
+                <div class="item-description">Ra mắt Atom Editor, giúp người dùng dễ dàng tạo, chỉnh sửa, lưu trữ và publish bài viết trên Viblo thông qua Atom Edifor.</div>
+            </div>
+            <div class="content-item">
+                <div class="item-title">10.07.2017</div>
+                <div class="item-description">Ra mắt tính n ăng Video Podcad, nhằm thúc đẩy việc chia sẻ kiến thức đa dạng, giúp người dùng h ọc tập dễ d àng hơn.</div>
+            </div>
+            <div class="content-item">
+                <div class="item-title">08.08.2017</div>
+                <div class="item-description">Ra mắt tính năng Question & Answer, giúp người dùng đặ t câu hỏi để nhận được trợ giúp từ thành viên khác.</div>
+            </div>
+            <div class="content-item">
+                <div class="item-title">26.11.2017</div>
+                <div class="item-description">Viblo Deployment Day, Sự kiện công nghệ đầu tiên của Viblo, do Viblo VỜ vì cộng đồng người dùng Viblo</div>
+            </div>
+            <div class="content-item">
+                <div class="item-title">23.04.2018</div>
+                <div class="item-description">Ra mắt ứng dụng Smartphone trên 2 nền tảng Android & iOS, giúp người dùng có thể dễ dàng tiếp cận với nguồn kiến thứ c phong phú đưỢC chia sẻ trên Viblo hàng ngày.</div>
+            </div>
+            <div class="content-item">
+                <div class="item-title">24.05.2018</div>
+                <div class="item-description">Thông báo về việc đồng hành cùng ngày hội công nghệ "Vietnam Mobile Day 2018" Với tư cách đơn vị bảo trợ truyền thông của Sự kiện . Các bài thuyết trình tại sự kiện được ghi hình và công bố trên Viblo Sau đó.</div>
+            </div>
+            <div class="content-item">
+                <div class="item-title">28.05.2018</div>
+                <div class="item-description">Open source nhiều services được sử dụng bởi Viblo tại địa chỉ https ://github.com/viblo-asia</div>
+            </div>
+            <div class="content-item">
+                <div class="item-title">31.05.2018</div>
+                <div class="item-description">Đạt cột mố c 1 triệu Pageview/ tháng, đánh dấu sự phát triển lớn mạnh của Viblo</div>
+            </div>
+            <div class="content-item">
+                <div class="item-title">26.06.2018</div>
+                <div class="item-description">Thông báo về việ c trở thành đơn vị bảo | trợ truyền thông của sự kiện OpenDev. Các bài thuyết trình tại sự kiện đư ợ c ghi hình và công bố trên Viblo Sau đó.</div>
+            </div>
+            <div class="content-item">
+                <div class="item-title">24.09.2018</div>
+                <div class="item-description">Ra mắt chuyên trang giới thiệu về các dịch vụ sử dụng Machine Learning của Viblo, tại địa chỉ https://machine-learning.violo.gsig, đồng thời thông báo về định hướng ứng dụng Machine Learning trong việc cải thiện trải nghiệm người dùng tại Viblo trong thời gian tới.</div>
+            </div>
+            <div class="content-item">
+                <div class="item-title">25.09.2018</div>
+                <div class="item-description">Thông báo về vi ệ c trở thành đơn vị bảo trợ truyền thông của sự kiện Blockchain lớn bậc nhất tại Vietnam, tiêu điểm Hackathon & Meetup "State of Chain 2018"....</div>
+            </div>
+            <div class="content-item">
+                <div class="item-title">2019</div>
+                <div class="item-description">Và còn nhiều, rất nhiều điều tuyệt Vời khác đang chờ đón Violo và các bạn trong năm 2019 sắp tới!</div>
+            </div>
+        </div>
+    </section>
+</template>
+<style lang="scss">
+@import '../scss/_variable.scss';
+$border_color: rgba(36, 61, 255, 1);
+$border_width: 3px;
+
+    #section-milstone {
+        .section-content {
+            display: flex;
+            flex-wrap: wrap;
+            justify-content: space-between;
+            .content-item {
+                width: 50%;
+                display: inline-block;
+                position: relative;
+                .item-title {
+                    color: #FFF;
+                    font-size: 36px;
+                    margin-bottom: 20px;
+                    display: inline-block;
+                    position: relative;
+                }
+                .item-title:before {
+                    content: "";
+                    display: block;
+                    width: 40px;
+                    height: 40px;
+                    background: #FFF;
+                    border: 6px solid $border_color;
+                    border-radius: 1000px;
+                    position: absolute;
+                    bottom: 0;
+                    z-index: 1;
+                }
+                .item-title:after {
+                    content: "";
+                    display: block;
+                    width: calc(100% + 50px);
+                    height: $border_width;
+                    background-color: #243DFF;
+                }
+                .item-description {
+                    color: $text_primary;
+                }
+            }
+            .content-item:nth-child(2n+1) {
+                text-align: right;
+                padding-right: 50px;
+                margin-bottom: 100px;
+                border-right: 6px solid $border_color;
+                right: -$border_width;
+                .item-title {}
+                .item-title:before {
+                    right: -50px;
+                    transform: translate(calc(50% + 3px), 50%);
+                }
+                .item-description {}
+            }
+            .content-item:nth-child(2n) {
+                text-align: left;
+                padding-left: 50px;
+                margin-top: 100px;
+                border-left: 6px solid $border_color;
+                left: -$border_width;
+                .item-title {}
+                .item-title:before {
+                    left: -50px;
+                    transform: translate(calc(-50% - 3px), 50%);
+                }
+                .item-title:after {
+                    margin-left: -50px;
+                }
+                .item-description {}
+            }
+        }
+    }
+</style>
