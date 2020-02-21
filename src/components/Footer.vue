180a3cc3d786905bb52f115a3dcc7b2b98115ebb
tranxuanthang <tran.xuan.thangb@sun-asterisk.com>
2020-02-21 15:20:10 +0700

    Add simple footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,15 +1,58 @@
 <template>
-    <footer class="text-center">
-      <div id="logo-footer"><img class="m-auto" src="../assets/logo-footer.svg" alt="viblo logo footer"></div>
-      <div class="slogan">
-        <span class="font-black">Free</span> service for technical knowledge <span class="font-black">sharing</span>
-      </div>
-      <div class="contact">
-        <span class="font-black">Website</span>: <a href="https://viblo.asia" target="_blank" rel="noopener noreferrer">http://viblo.asia</a><br/>
-        <span class="font-black">Facebook</span>: <a href="https://facebook.com/viblo.asia" target="_blank" rel="noopener noreferrer">/viblo.asia</a><br/>
-        <span class="font-black">Github</span>: <a href="https://github.com/viblo-asia" target="_blank" rel="noopener noreferrer">viblo-asia</a><br/>
-        <span class="font-black">Careers</span>: <a href="https://viblo.asia/careers" target="_blank" rel="noopener noreferrer">http://viblo.asia/careers</a><br/>
-        <span class="font-black">Machine Learning Services</span>: <a href="http://machine-learning.viblo.asia" target="_blank" rel="noopener noreferrer">http://machine-learning.viblo.asia</a>
+    <footer class="relative">
+      <div class="mx-auto max-w-screen-xl grid grid-cols-2 md:grid-cols-4 gap-6 p-5 items-center py-12 md:py-32">
+        <div class="col-span-2 md:col-auto md:row-span-2">
+          <img class="m-auto w-16 md:w-auto" src="../assets/logo-footer.svg" alt="Viblo logo footer">
+        </div>
+        <div class="link-box">
+          <div class="link-title">Website</div>
+          <div><a href="https://viblo.asia" target="_blank" rel="noopener noreferrer">viblo.asia</a></div>
+        </div>
+        <div class="link-box">
+          <div class="link-title">Facebook</div>
+          <div><a href="https://facebook.com/viblo.asia" target="_blank" rel="noopener noreferrer">fb.com/viblo.asia</a></div>
+        </div>
+        <div class="link-box">
+          <div class="link-title">Github</div>
+          <div><a href="https://github.com/viblo-asia" target="_blank" rel="noopener noreferrer">github.com/viblo-asia</a></div>
+        </div>
+        <div class="link-box">
+          <div class="link-title">Careers</div>
+          <div><a href="https://viblo.asia/careers" target="_blank" rel="noopener noreferrer">viblo.asia/careers</a></div>
+        </div>
+        <div class="link-box col-span-2 md:col-auto">
+          <div class="link-title">Machine Learning Services</div>
+          <div><a href="http://machine-learning.viblo.asia" target="_blank" rel="noopener noreferrer">machine-learning.viblo.asia</a></div>
+        </div>
       </div>
     </footer>
 </template>
+
+<style lang="scss" scoped>
+  footer {
+    &:before {
+      content: "";
+      background: url(../assets/v-pattern-footer.png) right bottom no-repeat;
+      background-size: contain;
+      mix-blend-mode: screen;
+      opacity: 0.5;
+      top: 0;
+      left: 0;
+      bottom: 0;
+      right: 0;
+      position: absolute;
+    }
+
+    .link-box {
+      @apply flex flex-col items-center col-span-2 z-10;
+
+      @screen md {
+        @apply items-start justify-center col-auto;
+      }
+
+      .link-title {
+        @apply text-blue-200 font-bold;
+      }
+    }
+  }
+</style>
