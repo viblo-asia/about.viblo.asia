594139c0c312540ec1fdcb36fe175bcbdd503613
Pham Van Hung <61034830+hungpv-2151@users.noreply.github.com>
2020-02-25 14:25:07 +0700

    refactor: reponsive section about, feature, milestone (#9)
    * change css to tailwind class, change the data content to script data
    
    * responsive SectionAbility and SectionAbout and SectionMilestome

--- src/components/SectionMilestone.vue
+++ src/components/SectionMilestone.vue
@@ -1,157 +1,189 @@
 <template>
-    <section id="section-milstone" class="text-center py-32 px-16">
-        <div class="section-head text-center">
-            <h3 class="uppercase text-4xl">Viblo <span class="font-black">milestone</span></h3>
-        </div>
-        <div class="section-content px-8 md:px-8">
-            <div class="content-item">
-                <div class="item-title">10.2014</div>
-                <div class="item-description">Dự án Viblo (Vietnam IT Blog) đưỢC khởi động với mụ c đích tạo ra một cộng đồng nơi thành viên có thể chia sẻ , họ c hỏi kiến thức Công nghệ | thông tin bằng tiếng mẹ đẻ.</div>
-            </div>
-            <div class="content-item">
-                <div class="item-title">03.11.2014</div>
-                <div class="item-description">Commit đầu tiên của dự án Viblo được thực hiện với nội dun g "First commit: Add Laravel 4.2"</div>
-            </div>
-            <div class="content-item">
-                <div class="item-title">03.04.2015</div>
-                <div class="item-description">Viblo chính thức được ra mắt.</div>
-            </div>
-            <div class="content-item">
-                <div class="item-title">10.2016</div>
-                <div class="item-description">Viblo được tái t ạo , sử dụn g công nghệ, n gôn ngữ thiết kế mới hơn, hiện đại hơn, phục vụ tốt hơn cho quá trình mở rộng.</div>
-            </div>
-            <div class="content-item">
-                <div class="item-title">29.12.2016</div>
-                <div class="item-description">Viblo Version 2 được ra mắt, thay thế hoàn toàn phiên bản cũ với logo, design được làm mới, và rất nhiều tính năng bổ sung.</div>
-            </div>
-            <div class="content-item">
-                <div class="item-title">24.03.2017</div>
-                <div class="item-description">Ra mắt Chrome Extension, giúp người dùn g dễ d àng tiếp cậ n nội dung, thông báo mới từ phía Viblo</div>
-            </div>
-            <div class="content-item">
-                <div class="item-title">15.05.2017</div>
-                <div class="item-description">Ra mắt Atom Editor, giúp người dùng dễ dàng tạo, chỉnh sửa, lưu trữ và publish bài viết trên Viblo thông qua Atom Edifor.</div>
-            </div>
-            <div class="content-item">
-                <div class="item-title">10.07.2017</div>
-                <div class="item-description">Ra mắt tính n ăng Video Podcad, nhằm thúc đẩy việc chia sẻ kiến thức đa dạng, giúp người dùng h ọc tập dễ d àng hơn.</div>
-            </div>
-            <div class="content-item">
-                <div class="item-title">08.08.2017</div>
-                <div class="item-description">Ra mắt tính năng Question & Answer, giúp người dùng đặ t câu hỏi để nhận được trợ giúp từ thành viên khác.</div>
-            </div>
-            <div class="content-item">
-                <div class="item-title">26.11.2017</div>
-                <div class="item-description">Viblo Deployment Day, Sự kiện công nghệ đầu tiên của Viblo, do Viblo VỜ vì cộng đồng người dùng Viblo</div>
-            </div>
-            <div class="content-item">
-                <div class="item-title">23.04.2018</div>
-                <div class="item-description">Ra mắt ứng dụng Smartphone trên 2 nền tảng Android & iOS, giúp người dùng có thể dễ dàng tiếp cận với nguồn kiến thứ c phong phú đưỢC chia sẻ trên Viblo hàng ngày.</div>
-            </div>
-            <div class="content-item">
-                <div class="item-title">24.05.2018</div>
-                <div class="item-description">Thông báo về việc đồng hành cùng ngày hội công nghệ "Vietnam Mobile Day 2018" Với tư cách đơn vị bảo trợ truyền thông của Sự kiện . Các bài thuyết trình tại sự kiện được ghi hình và công bố trên Viblo Sau đó.</div>
-            </div>
-            <div class="content-item">
-                <div class="item-title">28.05.2018</div>
-                <div class="item-description">Open source nhiều services được sử dụng bởi Viblo tại địa chỉ https ://github.com/viblo-asia</div>
-            </div>
-            <div class="content-item">
-                <div class="item-title">31.05.2018</div>
-                <div class="item-description">Đạt cột mố c 1 triệu Pageview/ tháng, đánh dấu sự phát triển lớn mạnh của Viblo</div>
-            </div>
-            <div class="content-item">
-                <div class="item-title">26.06.2018</div>
-                <div class="item-description">Thông báo về việ c trở thành đơn vị bảo | trợ truyền thông của sự kiện OpenDev. Các bài thuyết trình tại sự kiện đư ợ c ghi hình và công bố trên Viblo Sau đó.</div>
-            </div>
-            <div class="content-item">
-                <div class="item-title">24.09.2018</div>
-                <div class="item-description">Ra mắt chuyên trang giới thiệu về các dịch vụ sử dụng Machine Learning của Viblo, tại địa chỉ https://machine-learning.violo.gsig, đồng thời thông báo về định hướng ứng dụng Machine Learning trong việc cải thiện trải nghiệm người dùng tại Viblo trong thời gian tới.</div>
-            </div>
-            <div class="content-item">
-                <div class="item-title">25.09.2018</div>
-                <div class="item-description">Thông báo về vi ệ c trở thành đơn vị bảo trợ truyền thông của sự kiện Blockchain lớn bậc nhất tại Vietnam, tiêu điểm Hackathon & Meetup "State of Chain 2018"....</div>
-            </div>
-            <div class="content-item">
-                <div class="item-title">2019</div>
-                <div class="item-description">Và còn nhiều, rất nhiều điều tuyệt Vời khác đang chờ đón Violo và các bạn trong năm 2019 sắp tới!</div>
-            </div>
-        </div>
-    </section>
+  <section id="section-milstone" class="text-center py-32 md:px-16 max-w-full md:max-w-screen-xl m-auto">
+    <div class="section-head text-center">
+      <h3 class="uppercase text-4xl">Viblo <span class="font-black">milestone</span></h3>
+    </div>
+    <div class="section-content px-8 md:px-8 flex flex-wrap justify-between">
+      <div class="content-item w-full md:w-1/2 inline-block relative" v-for="data in dataEvents" :key="data.date">
+        <div class="item-title text-white text-4xl inline-block relative mb-6">{{ data.date }}</div>
+        <div class="item-description">{{ data.mess }}</div>
+      </div>
+    </div>
+  </section>
 </template>
+
+<script>
+export default {
+  data () {
+    return {
+      dataEvents: [
+        {
+          date: '10.2014',
+          mess: 'Dự án Viblo (Vietnam IT Blog) đưỢC khởi động với mụ c đích tạo ra một cộng đồng nơi thành viên có thể chia sẻ , họ c hỏi kiến thức Công nghệ | thông tin bằng tiếng mẹ đẻ.'
+        },
+        {
+          date: '03.11.2014',
+          mess: 'Commit đầu tiên của dự án Viblo được thực hiện với nội dun g "First commit: Add Laravel 4.2"'
+        },
+        {
+          date: '03.04.2015',
+          mess: 'Viblo chính thức được ra mắt.'
+        },
+        {
+          date: '10.2016',
+          mess: 'Viblo được tái t ạo , sử dụn g công nghệ, n gôn ngữ thiết kế mới hơn, hiện đại hơn, phục vụ tốt hơn cho quá trình mở rộng.'
+        },
+        {
+          date: '29.12.2016',
+          mess: 'Viblo Version 2 được ra mắt, thay thế hoàn toàn phiên bản cũ với logo, design được làm mới, và rất nhiều tính năng bổ sung.'
+        },
+        {
+          date: '24.03.2017',
+          mess: 'Ra mắt Chrome Extension, giúp người dùn g dễ d àng tiếp cậ n nội dung, thông báo mới từ phía Viblo'
+        },
+        {
+          date: '15.05.2017',
+          mess: 'Ra mắt Atom Editor, giúp người dùng dễ dàng tạo, chỉnh sửa, lưu trữ và publish bài viết trên Viblo thông qua Atom Edifor.'
+        },
+        {
+          date: '10.07.2017',
+          mess: 'Ra mắt tính n ăng Video Podcad, nhằm thúc đẩy việc chia sẻ kiến thức đa dạng, giúp người dùng h ọc tập dễ d àng hơn.'
+        },
+        {
+          date: '08.08.2017',
+          mess: 'Ra mắt tính năng Question & Answer, giúp người dùng đặ t câu hỏi để nhận được trợ giúp từ thành viên khác.'
+        },
+        {
+          date: '26.11.2017',
+          mess: 'Viblo Deployment Day, Sự kiện công nghệ đầu tiên của Viblo, do Viblo VỜ vì cộng đồng người dùng Viblo'
+        },
+        {
+          date: '23.04.2018',
+          mess: 'Ra mắt ứng dụng Smartphone trên 2 nền tảng Android & iOS, giúp người dùng có thể dễ dàng tiếp cận với nguồn kiến thứ c phong phú đưỢC chia sẻ trên Viblo hàng ngày.'
+        },
+        {
+          date: '24.05.2018',
+          mess: 'Thông báo về việc đồng hành cùng ngày hội công nghệ "Vietnam Mobile Day 2018" Với tư cách đơn vị bảo trợ truyền thông của Sự kiện . Các bài thuyết trình tại sự kiện được ghi hình và công bố trên Viblo Sau đó.'
+        },
+        {
+          date: '28.05.2018',
+          mess: 'Open source nhiều services được sử dụng bởi Viblo tại địa chỉ https ://github.com/viblo-asia'
+        },
+        {
+          date: '31.05.2018',
+          mess: 'Đạt cột mố c 1 triệu Pageview/ tháng, đánh dấu sự phát triển lớn mạnh của Viblo'
+        },
+        {
+          date: '26.06.2018',
+          mess: 'Thông báo về việ c trở thành đơn vị bảo | trợ truyền thông của sự kiện OpenDev. Các bài thuyết trình tại sự kiện đư ợ c ghi hình và công bố trên Viblo Sau đó.'
+        },
+        {
+          date: '24.09.2018',
+          mess: 'Ra mắt chuyên trang giới thiệu về các dịch vụ sử dụng Machine Learning của Viblo, tại địa chỉ https://machine-learning.violo.gsig, đồng thời thông báo về định hướng ứng dụng Machine Learning trong việc cải thiện trải nghiệm người dùng tại Viblo trong thời gian tới.'
+        },
+        {
+          date: '25.09.2018',
+          mess: 'Thông báo về vi ệ c trở thành đơn vị bảo trợ truyền thông của sự kiện Blockchain lớn bậc nhất tại Vietnam, tiêu điểm Hackathon & Meetup "State of Chain 2018"....'
+        },
+        {
+          date: '2019',
+          mess: 'Và còn nhiều, rất nhiều điều tuyệt Vời khác đang chờ đón Violo và các bạn trong năm 2019 sắp tới!'
+        }
+      ]
+    }
+  }
+}
+</script>
+
 <style lang="scss">
 @import '../scss/_variable.scss';
 $border_color: rgba(36, 61, 255, 1);
 $border_width: 3px;
 
-    #section-milstone {
-        .section-content {
-            display: flex;
-            flex-wrap: wrap;
-            justify-content: space-between;
-            .content-item {
-                width: 50%;
-                display: inline-block;
-                position: relative;
-                .item-title {
-                    color: #FFF;
-                    font-size: 36px;
-                    margin-bottom: 20px;
-                    display: inline-block;
-                    position: relative;
-                }
-                .item-title:before {
-                    content: "";
-                    display: block;
-                    width: 40px;
-                    height: 40px;
-                    background: #FFF;
-                    border: 6px solid $border_color;
-                    border-radius: 1000px;
-                    position: absolute;
-                    bottom: 0;
-                    z-index: 1;
-                }
-                .item-title:after {
-                    content: "";
-                    display: block;
-                    width: calc(100% + 50px);
-                    height: $border_width;
-                    background-color: #243DFF;
-                }
-                .item-description {
-                    color: $text_primary;
-                }
-            }
-            .content-item:nth-child(2n+1) {
-                text-align: right;
-                padding-right: 50px;
-                margin-bottom: 100px;
-                border-right: 6px solid $border_color;
-                right: -$border_width;
-                .item-title {}
-                .item-title:before {
-                    right: -50px;
-                    transform: translate(calc(50% + 3px), 50%);
-                }
-                .item-description {}
-            }
-            .content-item:nth-child(2n) {
-                text-align: left;
-                padding-left: 50px;
-                margin-top: 100px;
-                border-left: 6px solid $border_color;
-                left: -$border_width;
-                .item-title {}
-                .item-title:before {
-                    left: -50px;
-                    transform: translate(calc(-50% - 3px), 50%);
-                }
-                .item-title:after {
-                    margin-left: -50px;
-                }
-                .item-description {}
-            }
+  #section-milstone {
+    .section-content {
+      .content-item {
+        .item-title:before {
+          content: "";
+          display: block;
+          width: 40px;
+          height: 40px;
+          background: #FFF;
+          border: 6px solid $border_color;
+          border-radius: 1000px;
+          position: absolute;
+          bottom: 0;
+          z-index: 1;
         }
+        .item-title:after {
+          content: "";
+          display: block;
+          width: calc(100% + 50px);
+          height: $border_width;
+          background-color: #243DFF;
+        }
+        .item-description {
+          color: $text_primary;
+        }
+      }
+      .content-item:nth-child(2n+1) {
+        text-align: right;
+        padding-right: 50px;
+        margin-bottom: 100px;
+        border-right: 6px solid $border_color;
+        right: -$border_width;
+        .item-title:before {
+          right: -50px;
+          transform: translate(calc(50% + 3px), 50%);
+        }
+      }
+      .content-item:nth-child(2n) {
+        text-align: left;
+        padding-left: 50px;
+        margin-top: 100px;
+        border-left: 6px solid $border_color;
+        left: -$border_width;
+        .item-title:before {
+          left: -50px;
+          transform: translate(calc(-50% - 3px), 50%);
+        }
+        .item-title:after {
+          margin-left: -50px;
+        }
+      }
+      @media (max-width: 767px) {
+        .content-item {
+          padding: 15px 0 15px 30px;
+          .item-title:after {width: calc(100% + 30px)}
+        }
+        .content-item:nth-child(2n+1) {
+          margin: 0;
+          padding-right: 0;
+          border-right: none;
+          border-left: 6px solid $border_color;
+          right: auto;
+          left: 0;
+          text-align: left;
+          .item-title:before {
+            left: -30px;
+            transform: translate(calc(-50% - 3px), 50%);
+          }
+          .item-title:after {
+            margin-left: -30px;
+          }
+        }
+        .content-item:nth-child(2n) {
+          margin: 0;
+          padding-left: 30px;
+          left: 0;
+          .item-title:before {
+            left: -30px
+          }
+          .item-title:after {
+            margin-left: -30px;
+          }
+        }
+      }
     }
+  }
 </style>
