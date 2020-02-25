55b4d2f287f89029bd773b8617dbdd97a924c7a5
tranxuanthang <tran.xuan.thangb@sun-asterisk.com>
2020-02-25 14:22:35 +0700

    Data in separate files

---
+++ src/data/counter-items.js
@@ -0,0 +1,42 @@
+export default [
+  {
+    name: 'Người dùng active',
+    count: 20937
+  },
+  {
+    name: 'Bài viết được public',
+    count: 15218
+  },
+  {
+    name: 'Tag',
+    count: 4145
+  },
+  {
+    name: 'Lượt tương tác người dùng',
+    count: 87264
+  },
+  {
+    name: 'Bài viết được tương tác',
+    count: 11842
+  },
+  {
+    name: 'Seminar Videos',
+    count: 58
+  },
+  {
+    name: 'Lượt pageview hàng tháng',
+    count: 1100000
+  },
+  {
+    name: 'User mới hàng tháng',
+    count: 600
+  },
+  {
+    name: 'Bài viết mới hàng tháng',
+    count: 600
+  },
+  {
+    name: 'Câu hỏi được trả lời',
+    rate: 0.8
+  }
+]
---
+++ src/data/top-items.js
@@ -0,0 +1,30 @@
+export default [
+  {
+    title: 'Người dùng có <span class="font-bold">reputation</span> cao nhất',
+    belongs: 'Tran Duc Thang'
+  },
+  {
+    title: 'Người dùng có nhiều <span class="font-bold">bài viết</span> nhất',
+    belongs: 'Mona Media'
+  },
+  {
+    title: 'Bài viết được <span class="font-bold">vote up</span> nhiều nhất',
+    belongs: 'Docker: Chưa biết gì đến biết dùng (Phần 1- Lịch sử)'
+  },
+  {
+    title: 'Bài viết <span class="font-bold">được xem</span> nhiều nhất',
+    belongs: 'Những Plugin sublime text cần thiết cho web Developers'
+  },
+  {
+    title: 'Bài viết <span class="font-bold">được clip</span> nhiều nhất',
+    belongs: '19 Bí Kíp Bạn Có Thể Dùng Khi Phạm Sai Lầm Với Git'
+  },
+  {
+    title: '<span class="font-bold">Tag</span> được nhiều người dùng <span class="font-bold">follow</span> nhất',
+    belongs: 'Javascript'
+  },
+  {
+    title: '<span class="font-bold">Tag</span> có nhiều bài viết nhất',
+    belongs: 'Android'
+  }
+]
