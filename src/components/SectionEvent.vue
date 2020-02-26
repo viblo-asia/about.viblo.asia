2006bfe4e40a7202b6fe8e0582974e95edf171a0
Pham Van Hung <61034830+hungpv-2151@users.noreply.github.com>
2020-02-26 09:34:02 +0700

    refactor: section event (#11)
    * change css to tailwind class, change the data content to script data
    
    * responsive SectionAbility and SectionAbout and SectionMilestome
    
    * create section event and add slider, small fix responsive in other section
    
    * refactor: reponsive section about, feature, milestone (#9)
    
    * change css to tailwind class, change the data content to script data
    
    * responsive SectionAbility and SectionAbout and SectionMilestome
    
    * create section event and add slider, small fix responsive in other section
    
    * fix resposive in sectionability

---
+++ src/components/SectionEvent.vue
@@ -0,0 +1,100 @@
+<template>
+  <section id="section-event" class="text-center py-16 md:py-32 md:max-w-screen-xl m-auto">
+    <div class="about-images">
+      <Carousel3d :autoplay="true" :autoplay-timeout="5000" :display="5" :controls-visible="true" :perspective="0" :space="300" :height="heightString">
+        <slide v-for="(image, imageIndex) in dataImages" :key="image" :index="imageIndex">
+          <template slot-scope="{ index, isCurrent, leftIndex, rightIndex }">
+            <div class="item" ref="itemBox" :class="{ current: isCurrent, onLeft: (leftIndex >= 0), onRight: (rightIndex >= 0) }" :data-index="imageIndex">
+              <img :src="image" alt="slider" />
+              <div class="reflection h-56 overflow-hidden relative"><img :src="image" alt="slider" /></div>
+            </div>
+          </template>
+        </slide>
+      </Carousel3d>
+    </div>
+    <div class="about-content md:max-w-screen-md m-auto">
+      <div class="content-contact">
+        <p>TRONG NĂM 2018, VIBLO HÂN HẠNH ĐƯỢC ĐỒNG HÀNH VỚI VAI TRÒ ĐƠN VỊ BẢO VỆ TRUYỀN THÔNG CÙNG CÁC SỰ KIỆN IT HÀNG ĐẦU VIỆT NAM, THU HÚT HÀNG NGÀN LẬP TRÌNH VIÊN THAM DỰ.</p>
+        <br/>
+        <div class="contact">
+          State of Chain 2018 <br>
+          Vietnam Mobile Day 2018 <br>
+          Tech Invasion 2018 <br>
+          OpenDev: <br/>
+          "Inferring the Future of Mobile Application Development with AI/ML" <br/>
+          & "From Zero to Dapp PROgrammer"
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+import { Carousel3d, Slide } from 'vue-carousel-3d'
+
+export default {
+  components: {
+    Carousel3d,
+    Slide
+  },
+  methods: {
+    matchHeight () {
+      this.heightString = this.$refs.itemBox[0].clientHeight
+    }
+  },
+  mounted () {
+    window.setTimeout(() => {
+      this.matchHeight()
+    }, 300)
+  },
+  data () {
+    return {
+      dataImages: [
+        require('../assets/slide01.jpg'),
+        require('../assets/slide02.jpg'),
+        require('../assets/slide03.jpg'),
+        require('../assets/slide04.jpg'),
+        require('../assets/slide05.jpg'),
+        require('../assets/slide06.jpg'),
+        require('../assets/slide07.jpg')
+      ],
+      heightString: '320'
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+@import '../scss/_variable.scss';
+
+#section-event {
+  .carousel-3d-slide {
+    background: none;
+    border: none;
+    border-radius: 0;
+    padding: 30px
+  }
+  .item {
+    > img {box-shadow: 0px 0px 20px 10px #000}
+    .reflection {
+      img {
+        -webkit-transform: scaleY(-1);
+        transform: scaleY(-1);
+      }
+    }
+    .reflection::before {
+      content:"";
+      width:100%;
+      height: 100%;
+      position: absolute;
+      bottom: 0;
+      left: 0;
+      background: linear-gradient(to bottom, rgba(2, 0, 46,0.7),#02002E);
+      z-index: 1;
+    }
+  }
+  .carousel-3d-controls {
+    .prev, .next {color: #FFF}
+  }
+}
+</style>
