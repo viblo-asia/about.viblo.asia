dbc1aa12313c8f999c784ea7940704341852d449
tranxuanthang <tran.xuan.thangb@sun-asterisk.com>
2020-02-26 14:29:41 +0700

    Make footer similar to Viblo

--- src/components/FirstSection.vue
+++ src/components/FirstSection.vue
@@ -1,9 +1,9 @@
 <template>
   <section id="first-section"
-    class="relative near-full-height">
-    <div id="brand" class="flex flex-col absolute z-10">
+    class="relative py-32 md:py-0 md:h-screen">
+    <div id="brand" class="flex flex-col items-center p-10 max-w-md mx-auto md:max-w-lg md:absolute z-10">
       <img src="../assets/logo.svg" alt="Viblo Logo" id="logo" class="mb-3">
-      <div class="text-center select-auto text-lg md:text-2xl"><strong>Free</strong> service for technical knowledge <strong>sharing</strong></div>
+      <div class="text-blue-300 text-center select-auto text-lg md:text-2xl md:whitespace-no-wrap"><strong>Free</strong> service for technical knowledge <strong>sharing</strong></div>
     </div>
   </section>
 </template>
@@ -11,7 +11,6 @@
 <style lang="scss" scoped>
   #first-section {
     background: radial-gradient(#003dbd, #01001e);
-    height: 90vh;
 
     &:after {
       content: "";
@@ -25,20 +24,11 @@
       position: absolute;
     }
 
-    #brand {
-      left: 50%;
-      top: 32%;
-      transform: translateX(-50%);
-      color: #7acaff;
-    }
-  }
-
-  @media (max-width: 800px) {
-    #first-section {
-      height: 45vh;
-
+    @screen md {
       #brand {
         left: 50%;
+        top: 50%;
+        transform: translate(-50%, -50%);
       }
     }
   }
