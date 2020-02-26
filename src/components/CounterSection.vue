a20128519711913c4669f0415e74f707d177b205
thangbtx-0846 <47617090+thangbtx-0846@users.noreply.github.com>
2020-02-26 15:09:16 +0700

    refactor: data in separate files (#13)

--- src/components/CounterSection.vue
+++ src/components/CounterSection.vue
@@ -22,51 +22,12 @@
 </template>
 
 <script>
+import counterItems from '@/data/counter-items'
+
 export default {
   data () {
     return {
-      dataCounters: [
-        {
-          name: 'Người dùng active',
-          count: 20937
-        },
-        {
-          name: 'Bài viết được public',
-          count: 15218
-        },
-        {
-          name: 'Tag',
-          count: 4145
-        },
-        {
-          name: 'Lượt tương tác người dùng',
-          count: 87264
-        },
-        {
-          name: 'Bài viết được tương tác',
-          count: 11842
-        },
-        {
-          name: 'Seminar Videos',
-          count: 58
-        },
-        {
-          name: 'Lượt pageview hàng tháng',
-          count: 1100000
-        },
-        {
-          name: 'User mới hàng tháng',
-          count: 600
-        },
-        {
-          name: 'Bài viết mới hàng tháng',
-          count: 600
-        },
-        {
-          name: 'Câu hỏi được trả lời',
-          rate: 0.8
-        }
-      ]
+      dataCounters: counterItems
     }
   },
 
