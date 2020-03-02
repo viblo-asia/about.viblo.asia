e15d31c9ad6de5f132379bcbc0ae415275cc5e6f
thangbtx-0846 <47617090+thangbtx-0846@users.noreply.github.com>
2020-03-02 13:01:32 +0700

    fix: header fits screen, ML section background fix (#30)
    Header section always has min-height is 100vh, both mobile and large
    screen
    Fix ML section background

--- src/components/FirstSection.vue
+++ src/components/FirstSection.vue
@@ -1,9 +1,11 @@
 <template>
   <section id="first-section"
-    class="relative py-32 md:py-0 md:h-screen">
+    class="relative py-32 md:py-0 min-h-screen flex items-center">
     <div id="brand" class="flex flex-col items-center p-10 max-w-md mx-auto md:max-w-lg md:absolute z-10">
       <img src="../assets/logo.svg" alt="Viblo Logo" id="logo" class="mb-3">
-      <div class="text-blue-300 text-center select-auto text-lg md:text-2xl md:whitespace-no-wrap"><strong>Free</strong> service for technical knowledge <strong>sharing</strong></div>
+      <div class="text-blue-300 text-center select-auto text-lg md:text-2xl md:whitespace-no-wrap">
+        <h1><strong>Ultimate Platform</strong> for Learning and Sharing <strong>IT Knowledge</strong></h1>
+      </div>
     </div>
   </section>
 </template>
@@ -30,6 +32,12 @@
         top: 50%;
         transform: translate(-50%, -50%);
       }
+    }
+  }
+
+  @responsive {
+    .min-h-screen {
+      min-height: 100vh;
     }
   }
 </style>
