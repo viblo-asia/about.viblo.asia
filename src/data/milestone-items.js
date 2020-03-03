a7369125ed7055ddde9179b6c4f8d6d088e7cbcf
Pham Van Hung <61034830+hungpv-2151@users.noreply.github.com>
2020-03-03 15:15:19 +0700

    refactor: move milestone's data to file, add more data (#38)

---
+++ src/data/milestone-items.js
@@ -0,0 +1,102 @@
+export default [
+  {
+    date: '10.2014',
+    mess: 'Dự án Viblo (Vietnam IT Blog) được khởi động với mục đích tạo ra một cộng đồng nơi thành viên có thể chia sẻ, học hỏi kiến thức Công nghệ thông tin bằng tiếng mẹ đẻ.'
+  },
+  {
+    date: '03.11.2014',
+    mess: 'Commit đầu tiên của dự án Viblo được thực hiện với nội dung "First commit: Add Laravel 4.2"'
+  },
+  {
+    date: '03.04.2015',
+    mess: 'Viblo chính thức được ra mắt.'
+  },
+  {
+    date: '10.2016',
+    mess: 'Viblo được tái tạo, sử dụng công nghệ, ngôn ngữ thiết kế mới hơn, hiện đại hơn, phục vụ tốt hơn cho quá trình mở rộng.'
+  },
+  {
+    date: '29.12.2016 ',
+    mess: 'Viblo Version 2 được ra mắt, thay thế hoàn toàn phiên bản cũ với logo, design được làm mới, và rất nhiều tính năng bổ sung.'
+  },
+  {
+    date: '24.03.2017',
+    mess: 'Ra mắt Chrome Extension, giúp người dùng dễ dàng tiếp cận nội dung, thông báo mới từ phía Viblo.'
+  },
+  {
+    date: '15.05.2017',
+    mess: 'Ra mắt Atom Editor, giúp người dùng dễ dàng tạo, chỉnh sửa, lưu trữ và publish bài viết trên Viblo thông qua Atom Editor.'
+  },
+  {
+    date: '10.07.2017',
+    mess: 'Ra mắt tính năng Video Podcad, nhằm thúc đẩy việc chia sẻ kiến thức đa dạng, giúp người dùng học tập dễ dàng hơn.'
+  },
+  {
+    date: '08.08.2017',
+    mess: 'Ra mắt tính năng Question & Answer, giúp người dùng đặt câu hỏi để nhận được trợ giúp từ thành viên khác.'
+  },
+  {
+    date: '26.11.2017',
+    mess: 'Viblo Deployment Day, sự kiện công nghệ đầu tiên của Viblo, do Viblo và vì cộng đồng người dùng Viblo.'
+  },
+  {
+    date: '23.04.2018',
+    mess: 'Ra mắt ứng dụng Smartphone trên 2 nền tảng Android & iOS, giúp người dùng có thể dễ dàng tiếp cận với nguồn kiến thức phong phú được chia sẻ trên Viblo hàng ngày.'
+  },
+  {
+    date: '28.05.2018',
+    mess: 'Open source nhiều services được sử dụng bởi Viblo tại địa chỉ https://github.com/viblo-asia'
+  },
+  {
+    date: '31.05.2018',
+    mess: 'Đạt cột mốc 1 triệu Pageview/tháng, đánh dấu sự phát triển lớn mạnh của Viblo.'
+  },
+  {
+    date: '26.06.2018',
+    mess: 'Thông báo về việc trở thành đơn vị bảo trợ truyền thông của sự kiện OpenDev. Các bài thuyết trình tại sự kiện được ghi hình và công bố trên Viblo sau đó.'
+  },
+  {
+    date: '24.09.2018',
+    mess: 'Ra mắt chuyên trang giới thiệu về các dịch vụ sử dụng Machine Learning của Viblo, tại địa chỉ https://machine-learning.viblo.asia, đồng thời thông báo về định hướng ứng dụng Machine Learning trong việc cải thiện trải nghiệm người dùng tại Viblo trong thời gian tới.'
+  },
+  {
+    date: '25.09.2018',
+    mess: 'Thông báo về việc trở thành đơn vị bảo trợ truyền thông của sự kiện Blockchain lớn bậc nhất tại Vietnam, tiêu điểm Hackathon & Meetup "State of Chain 2018"...'
+  },
+  {
+    date: '10.2018',
+    mess: 'Viblo chuyển đổi định hướng hoạt động từ một service thành một Platform: Viblo Platform, chuẩn bị khai sinh ra các service mới gia nhập hệ sinh thái của Viblo.'
+  },
+  {
+    date: '20.02.2019',
+    mess: 'Ra mắt chức năng "Organization" trên Viblo, giúp các team, công ty có thể chia sẻ về những sứ mệnh, tầm nhìn và những kinh nghiệm làm việc của mình trong ngành IT.'
+  },
+  {
+    date: '11.03.2019',
+    mess: 'Viblo chính thức ra mắt dịch vụ mới: Viblo Code!'
+  },
+  {
+    date: '13.03.2019',
+    mess: 'Mở đăng ký sự kiện Code Challenge - Sân chơi lập trình hấp dẫn dành cho sinh viên IT miền Trung'
+  },
+  {
+    date: '20.04.2019',
+    mess: 'Tổ chức thành công chung kết cuộc thi Code Challenge 2019, sau vòng loại đã tổ chức ngày 11.04.2019'
+  },
+  {
+    date: '31.03.2019',
+    mess: 'Cán mốc 1.5 triệu pageview/tháng'
+  },
+  {
+    date: '23.05.2019',
+    mess: 'Viblo chính thức ra mắt dịch vụ mới: Viblo CV!'
+  },
+  {
+    date: '03.10.2019',
+    mess: 'Viblo Platform: chính thức ra mắt Viblo CTF!'
+  },
+  {
+    date: '03.04.2020',
+    mess: 'Khởi động chuỗi sự kiện kỷ niệm 5 năm ngày ra mắt chính thức của Viblo. Mở hệ thống đăng ký các cuộc thi Viblo Mayfest, Code Contest và CTF Contest.'
+  }
+]
