2411adda06b6fa7f322a963229f1eeaedfe2bff1
Nguyen Huu Kim <kimnguyen.ict@gmail.com>
2020-03-11 11:29:31 +0700

    data: update new data

--- src/data/top-items.js
+++ src/data/top-items.js
@@ -1,30 +1,37 @@
 export default [
   {
     title: 'Người dùng có <span class="font-bold">reputation</span> cao nhất',
-    belongs: 'Tran Duc Thang'
+    belongs: 'Tran Duc Thang',
+    url: 'https://viblo.asia/u/thangtd90'
   },
   {
     title: 'Người dùng có nhiều <span class="font-bold">bài viết</span> nhất',
-    belongs: 'Mona Media'
+    belongs: 'Cuong Le Ngoc @lncnb',
+    url: 'https://viblo.asia/u/lncnb'
   },
   {
     title: 'Bài viết được <span class="font-bold">vote up</span> nhiều nhất',
-    belongs: 'Docker: Chưa biết gì đến biết dùng (Phần 1- Lịch sử)'
+    belongs: 'Docker: Chưa biết gì đến biết dùng (Phần 1- Lịch sử)',
+    url: 'https://viblo.asia/p/docker-chua-biet-gi-den-biet-dung-phan-1-lich-su-ByEZkWrEZQ0'
   },
   {
     title: 'Bài viết <span class="font-bold">được xem</span> nhiều nhất',
-    belongs: 'Những Plugin sublime text cần thiết cho web Developers'
+    belongs: 'Phân tích thiết kế hệ thống thông tin sử dụng biểu đồ UML (Phần 1)',
+    url: 'https://viblo.asia/p/phan-tich-thiet-ke-he-thong-thong-tin-su-dung-bieu-do-uml-phan-1-PjxMe6yNG4YL'
   },
   {
     title: 'Bài viết <span class="font-bold">được clip</span> nhiều nhất',
-    belongs: '19 Bí Kíp Bạn Có Thể Dùng Khi Phạm Sai Lầm Với Git'
+    belongs: 'Học Regular Expression và cuộc đời bạn sẽ bớt khổ (Updated v2.2)',
+    url: 'https://viblo.asia/p/hoc-regular-expression-va-cuoc-doi-ban-se-bot-kho-updated-v22-Az45bnoO5xY'
   },
   {
     title: '<span class="font-bold">Tag</span> được nhiều người dùng <span class="font-bold">follow</span> nhất',
-    belongs: 'Javascript'
+    belongs: 'JavaScript',
+    url: 'https://viblo.asia/tags/javascript'
   },
   {
     title: '<span class="font-bold">Tag</span> có nhiều bài viết nhất',
-    belongs: 'Android'
+    belongs: 'Ruby on Rails',
+    url: 'https://viblo.asia/tags/ruby-on-rails'
   }
 ]
