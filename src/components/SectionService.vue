ae57ff534e26af1a9c713554dec3e2db64ec5b95
Nguyen Huu Kim <kimnguyen.ict@gmail.com>
2020-03-20 11:49:34 +0700

    refactor: udpate service's introduction

--- src/components/SectionService.vue
+++ src/components/SectionService.vue
@@ -1,13 +1,27 @@
 <template>
-  <section id="section-service" class="scrollspy text-center py-16 bg-cover bg-right-top bg-no-repeat">
+  <section
+    id="section-service"
+    class="scrollspy text-center py-16 bg-cover bg-right-top bg-no-repeat"
+  >
     <div class="md:max-w-screen-xl m-auto px-4">
       <VueAos animation-class="puffIn magictime" :threshold="1">
-        <TitleSection section_title='Our <span class="font-black">services</span>' />
+        <TitleSection section_title="Our <span class='font-black'>services</span>" />
       </VueAos>
       <div class="services-list text-white px-4">
-        <div class="service-item py-20 md:py-16 flex flex-row items-center relative" v-for="(service, index) in dataServices" :key="index" :class="service.code">
-          <VueAos :animation-class="index % 2 === 0 ? 'slideLeftReturn magictime' : 'slideRightReturn magictime'">
-            <div class="item-images absolute sm:relative overflow-hidden sm:block sm:w-1/3 md:w-1/2 inset-0 flex items-center justify-center" :class="index % 2 === 0 ? '' : 'md:order-last'">
+        <div
+          v-for="(service, index) in dataServices"
+          :key="index"
+          :class="service.code"
+          class="service-item py-20 md:py-16 flex flex-row items-center relative"
+          style="min-height: 70vh"
+        >
+          <VueAos
+            :animation-class="index % 2 === 0 ? 'slideLeftReturn magictime' : 'slideRightReturn magictime'"
+          >
+            <div
+              class="item-images absolute sm:relative overflow-hidden sm:block sm:w-1/3 md:w-1/2 inset-0 flex items-center justify-center"
+              :class="index % 2 === 0 ? '' : 'md:order-last'"
+            >
               <div class="images py-8 md:py-0 h-full">
                 <!-- <div class="image"><img :src="service.logo" :alt="service.title" /></div> -->
                 <div class="image h-full" v-for="image in service.images" :key="image">
@@ -16,19 +30,35 @@
               </div>
             </div>
           </VueAos>
-          <VueAos :animation-class="index % 2 !== 0 ? 'slideLeftReturn magictime' : 'slideRightReturn magictime'">
-            <div class="item-content w-full sm:w-2/3 md:w-1/2 text-center sm:text-left sm:pl-4 md:pl-0 z-10" :class="index % 2 !== 0 ? 'md:order-first' : 'md:text-right'">
+          <VueAos
+            :animation-class="index % 2 !== 0 ? 'slideLeftReturn magictime' : 'slideRightReturn magictime'"
+          >
+            <div
+              class="item-content w-full sm:w-2/3 md:w-1/2 text-center sm:text-left sm:pl-4 md:pl-0 z-10"
+              :class="index % 2 !== 0 ? 'md:order-first' : 'md:text-right'"
+            >
               <div class="logo">
-                <img class="inline-block max-w-full md:max-w-lg md:h-20" :src="service.logo" :alt="service.title" />
+                <img
+                  class="inline-block max-w-full md:max-w-lg md:h-20"
+                  :src="service.logo"
+                  :alt="service.title"
+                />
               </div>
 
               <div class="title text-2xl lg:text-4xl font-bold hidden">
                 <h3 class="uppercase">{{ service.title }}</h3>
               </div>
 
-              <div class="description text-lg lg:text-2xl my-8 lg:my-16" v-html="service.description"></div>
+              <div
+                class="description text-lg lg:text-2xl my-8 lg:my-16"
+                v-html="service.description"
+              ></div>
               <div class="link">
-                <a :href="service.link" class="button rounded-full py-2 px-4 sm:px-16 inline-block" target="_blank">Go to {{ service.title }}</a>
+                <a
+                  :href="service.link"
+                  class="button rounded-full py-2 px-4 sm:px-16 inline-block"
+                  target="_blank"
+                >Go to {{ service.title }}</a>
               </div>
             </div>
           </VueAos>
@@ -49,45 +79,45 @@ export default {
         {
           title: 'Viblo',
           code: 'v-viblo',
-          description: '<h4 class="uppercase font-bold title">Learning to code?<br>Preparing for an interview?</h4><p>Or just want to have fun with challenging problems?</p>',
+          description:
+            'Dịch vụ miễn phí cho phép người dùng đọc, đăng tải các bài viết, chia sẻ kinh nghiệm làm việc' +
+            ' hay đặt câu hỏi nhờ giúp đỡ trong lĩnh vực công nghệ thông tin.',
           logo: require('../assets/logo.svg'),
-          link: 'javascript:void(0)',
-          images: [
-            require('../assets/code-image.png')
-            // require('../assets/code-bg.png')
-          ]
+          link: 'https://viblo.asia',
+          images: [require('../assets/code-image.png')]
         },
         {
           title: 'Viblo Code',
           code: 'v-code',
-          description: '<h4 class="uppercase font-bold title">Learning to code?<br>Preparing for an interview?</h4><p>Or just want to have fun with challenging problems?</p>',
+          description:
+            'Tham gia Viblo Code để thử sức bản thân tự làm ra các chương trình để giải quyết cho' +
+            ' những bài toán đầy thách thức về thuật toán và tối ưu hóa code của Viblo.',
           logo: require('../assets/code-logo.png'),
-          link: 'javascript:void(0)',
-          images: [
-            require('../assets/code-image.png')
-            // require('../assets/code-bg.png')
-          ]
+          link: 'https://code.viblo.asia',
+          images: [require('../assets/code-image.png')]
         },
         {
           title: 'Viblo CTF',
           code: 'v-ctf',
-          description: '<h4 class="uppercase font-bold title">WANT TO CHALLENGE YOUR SKILL?<br>DARE TO FACE HARDCORE PUZZLE?</h4><p>Refresh your mind after hard-working times?</p>',
+          description:
+            'CTF - một loại cuộc thi về lĩnh vực bảo mật thông tin danh cho các hacker.' +
+            ' Bằng cách nào đấy, bạn phải tìm ra một đoạn văn bản bí mật nào đó (flag),' +
+            ' đang bị ẩn giấu sau một trong web, hay trên một máy chủ của Viblo CTF.' +
+            ' Tham gia Viblo CTF để thử sức ngay nào!',
           logo: require('../assets/ctf-logo.png'),
-          link: 'javascript:void(0)',
-          images: [
-            require('../assets/ctf-image.png')
-            // require('../assets/ctf-bg.png')
-          ]
+          link: 'https://ctf.viblo.asia',
+          images: [require('../assets/ctf-image.png')]
         },
         {
           title: 'Viblo CV',
           code: 'v-cv',
-          description: '<h4 class="uppercase font-bold title">A GOOD CV makes your life EASIER</h4><p>whereas a Bad CV makes it a disaster</p>',
+          description:
+            'Dịch vụ cho phép bạn tạo những bản CV online hoàn toàn miễn phí' +
+            ' theo như ý thích của mình dựa trên nhiều mẫu cv đẹp, hiện đại và đảm bảo tính riêng tư.' +
+            ' Viblo CV hoàn toàn không bán, hay chia sẻ dữ liệu người dùng với các công ty tuyển dụng.',
           logo: require('../assets/cv-logo.svg'),
-          link: 'javascript:void(0)',
-          images: [
-            require('../assets/cv-image.png')
-          ]
+          link: 'https://cv.viblo.asia',
+          images: [require('../assets/cv-image.png')]
         }
       ]
     }
@@ -102,24 +132,40 @@ export default {
 
 <style lang="scss">
 #section-service {
-  background: -moz-linear-gradient(top,  rgba(2,0,46,1) 0%, rgba(2,0,46,0) 100%); /* FF3.6-15 */
-  background: -webkit-linear-gradient(top,  rgba(2,0,46,1) 0%,rgba(2,0,46,0) 100%); /* Chrome10-25,Safari5.1-6 */
-  background: linear-gradient(to bottom,  rgba(2,0,46,1) 0%,rgba(2,0,46,0) 100%); /* W3C, IE10+, FF16+, Chrome26+, Opera12+, Safari7+ */
+  background: -moz-linear-gradient(
+    top,
+    rgba(2, 0, 46, 1) 0%,
+    rgba(2, 0, 46, 0) 100%
+  ); /* FF3.6-15 */
+  background: -webkit-linear-gradient(
+    top,
+    rgba(2, 0, 46, 1) 0%,
+    rgba(2, 0, 46, 0) 100%
+  ); /* Chrome10-25,Safari5.1-6 */
+  background: linear-gradient(
+    to bottom,
+    rgba(2, 0, 46, 1) 0%,
+    rgba(2, 0, 46, 0) 100%
+  ); /* W3C, IE10+, FF16+, Chrome26+, Opera12+, Safari7+ */
   filter: progid:DXImageTransform.Microsoft.gradient( startColorstr='#02002e', endColorstr='#0002002e',GradientType=0 ); /* IE6-9 */
 
   .service-item {
+    min-height: 100vh;
+    @media screen and (min-width: 1600px) {
+      min-heigth: 70vh;
+    }
     .item-images {
       @media (max-width: 640px) {
         :after {
-          content: '';
+          content: "";
           width: 100%;
           height: 100%;
-          background-color: rgba(1, 0, 45, .6);
+          background-color: rgba(1, 0, 45, 0.6);
           display: block;
           position: absolute;
           top: 0;
           left: 0;
-          z-index: 1
+          z-index: 1;
         }
       }
       .images {
@@ -129,23 +175,28 @@ export default {
       }
     }
     .item-content {
-      @media (max-width: 640px) {
-        // background-color: rgba(0,0,0,.7)
-      }
       .logo {
-        img {max-height: 5rem}
+        img {
+          max-height: 5rem;
+        }
       }
       .link .button {
         background-color: #3ec08d;
       }
     }
   }
-  .service-item.v-viblo .link .button {background-color: #3ec08d;}
-  .service-item.v-code .link .button {background-color: #3E6F8D}
-  .service-item.v-ctf .link .button {
-    background-color: #97F739;
-    color: #F05D32;
+  .service-item.v-viblo .link .button {
+    background-color: #3ec08d;
   }
-  .service-item.v-cv .link .button {background-color: #F5861E}
+  .service-item.v-code .link .button {
+    background-color: #3e6f8d;
+  }
+  .service-item.v-ctf .link .button {
+    background-color: #97f739;
+    color: #f05d32;
+  }
+  .service-item.v-cv .link .button {
+    background-color: #f5861e;
+  }
 }
 </style>
--- src/data/top-items.js
+++ src/data/top-items.js
@@ -1,11 +1,11 @@
 export default [
   {
-    title: 'Người dùng có <span class="font-bold">reputation</span> cao nhất',
+    title: 'Người dùng có <strong>reputation</strong> cao nhất',
     belongs: 'Tran Duc Thang <small>@thangtd90<small/>',
     url: 'https://viblo.asia/u/thangtd90'
   },
   {
-    title: 'Người dùng cao điểm nhất Viblo Code',
+    title: 'Người dùng <strong>cao điểm nhất<strong> Viblo Code',
     belongs: 'king <small>@knight<small/>',
     url: 'https://code.viblo.asia/users/knight'
   },
@@ -20,32 +20,32 @@ export default [
     url: 'https://viblo.asia/o/sun-blockchain-team'
   },
   {
-    title: 'Người dùng có nhiều <span class="font-bold">bài viết</span> nhất',
+    title: 'Người dùng có nhiều <strong>bài viết</strong> nhất',
     belongs: 'Cuong Le Ngoc <small>@lncnb</small>',
     url: 'https://viblo.asia/u/lncnb'
   },
   {
-    title: 'Bài viết được <span class="font-bold">vote up</span> nhiều nhất',
+    title: 'Bài viết được <strong>vote up</strong> nhiều nhất',
     belongs: 'Docker: Chưa biết gì đến biết dùng (Phần 1- Lịch sử)',
     url: 'https://viblo.asia/p/docker-chua-biet-gi-den-biet-dung-phan-1-lich-su-ByEZkWrEZQ0'
   },
   {
-    title: 'Bài viết <span class="font-bold">được xem</span> nhiều nhất',
+    title: 'Bài viết <strong>được xem</strong> nhiều nhất',
     belongs: 'Phân tích thiết kế hệ thống thông tin sử dụng biểu đồ UML (Phần 1)',
     url: 'https://viblo.asia/p/phan-tich-thiet-ke-he-thong-thong-tin-su-dung-bieu-do-uml-phan-1-PjxMe6yNG4YL'
   },
   {
-    title: 'Bài viết <span class="font-bold">được clip</span> nhiều nhất',
+    title: 'Bài viết <strong>được clip</strong> nhiều nhất',
     belongs: 'Học Regular Expression và cuộc đời bạn sẽ bớt khổ (Updated v2.2)',
     url: 'https://viblo.asia/p/hoc-regular-expression-va-cuoc-doi-ban-se-bot-kho-updated-v22-Az45bnoO5xY'
   },
   {
-    title: '<span class="font-bold">Tag</span> được nhiều người dùng <span class="font-bold">follow</span> nhất',
+    title: '<strong>Tag</strong> được nhiều người dùng <strong>follow</strong> nhất',
     belongs: 'JavaScript',
     url: 'https://viblo.asia/tags/javascript'
   },
   {
-    title: '<span class="font-bold">Tag</span> có nhiều bài viết nhất',
+    title: '<strong>Tag</strong> có nhiều bài viết nhất',
     belongs: 'Ruby on Rails',
     url: 'https://viblo.asia/tags/ruby-on-rails'
   }
