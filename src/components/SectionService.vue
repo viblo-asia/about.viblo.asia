2ef4a2b96d8aea845acc4ca1f6f7001bab5c2247
Nguyen Huu Kim <kimnguyen.ict@gmail.com>
2020-03-26 08:58:46 +0700

    refactor: update services content

--- src/components/SectionService.vue
+++ src/components/SectionService.vue
@@ -71,57 +71,10 @@
 <script>
 import VueAos from 'vue-aos'
 import TitleSection from './TitleSection'
+import dataServices from '@/data/services'
 
 export default {
-  data () {
-    return {
-      dataServices: [
-        {
-          title: 'Viblo',
-          code: 'v-viblo',
-          description:
-            'Dịch vụ miễn phí cho phép người dùng đọc, đăng tải các bài viết, chia sẻ kinh nghiệm làm việc' +
-            ' hay đặt câu hỏi nhờ giúp đỡ trong lĩnh vực công nghệ thông tin.',
-          logo: require('../assets/logo.svg'),
-          link: 'https://viblo.asia',
-          images: [require('../assets/code-image.png')]
-        },
-        {
-          title: 'Viblo Code',
-          code: 'v-code',
-          description:
-            'Tham gia Viblo Code để thử sức bản thân tự làm ra các chương trình để giải quyết cho' +
-            ' những bài toán đầy thách thức về thuật toán và tối ưu hóa code của Viblo.',
-          logo: require('../assets/code-logo.png'),
-          link: 'https://code.viblo.asia',
-          images: [require('../assets/code-image.png')]
-        },
-        {
-          title: 'Viblo CTF',
-          code: 'v-ctf',
-          description:
-            'CTF - một loại cuộc thi về lĩnh vực bảo mật thông tin danh cho các hacker.' +
-            ' Bằng cách nào đấy, bạn phải tìm ra một đoạn văn bản bí mật nào đó (flag),' +
-            ' đang bị ẩn giấu sau một trong web, hay trên một máy chủ của Viblo CTF.' +
-            ' Tham gia Viblo CTF để thử sức ngay nào!',
-          logo: require('../assets/ctf-logo.png'),
-          link: 'https://ctf.viblo.asia',
-          images: [require('../assets/ctf-image.png')]
-        },
-        {
-          title: 'Viblo CV',
-          code: 'v-cv',
-          description:
-            'Dịch vụ cho phép bạn tạo những bản CV online hoàn toàn miễn phí' +
-            ' theo như ý thích của mình dựa trên nhiều mẫu cv đẹp, hiện đại và đảm bảo tính riêng tư.' +
-            ' Viblo CV hoàn toàn không bán, hay chia sẻ dữ liệu người dùng với các công ty tuyển dụng.',
-          logo: require('../assets/cv-logo.svg'),
-          link: 'https://cv.viblo.asia',
-          images: [require('../assets/cv-image.png')]
-        }
-      ]
-    }
-  },
+  data: () => ({ dataServices }),
 
   components: {
     VueAos,
---
+++ src/data/services.js
@@ -0,0 +1,46 @@
+export default [
+  {
+    title: 'Viblo',
+    code: 'v-viblo',
+    description:
+      'Dịch vụ miễn phí cho phép người dùng đọc bài, đăng tải bài viết,' +
+      ' chia sẻ kinh nghiệm làm việc hoặc là đặt câu hỏi thuộc lĩnh vực công nghệ thông tin' +
+      ' để  nhận sự giúp đỡ từ chính cộng đồng người dùng của Viblo.',
+    logo: require('../assets/logo.svg'),
+    link: 'https://viblo.asia',
+    images: [require('../assets/viblo-poster.png')]
+  },
+  {
+    title: 'Viblo Code',
+    code: 'v-code',
+    description:
+      'Tham gia Viblo Code để thử sức bản thân tự làm ra các chương trình để giải quyết cho' +
+      ' những bài toán đầy thách thức về thuật toán và tối ưu hóa code của Viblo.',
+    logo: require('../assets/code-logo.png'),
+    link: 'https://code.viblo.asia',
+    images: [require('../assets/code-image.png')]
+  },
+  {
+    title: 'Viblo CTF',
+    code: 'v-ctf',
+    description:
+      'CTF - một loại cuộc thi về lĩnh vực bảo mật thông tin danh cho các hacker.' +
+      ' Bằng cách nào đấy, bạn phải tìm ra một đoạn văn bản bí mật nào đó (flag),' +
+      ' đang bị ẩn giấu sau một trong web, hay trên một máy chủ của Viblo CTF.' +
+      ' Tham gia Viblo CTF để thử sức ngay nào!',
+    logo: require('../assets/ctf-logo.png'),
+    link: 'https://ctf.viblo.asia',
+    images: [require('../assets/ctf-image.png')]
+  },
+  {
+    title: 'Viblo CV',
+    code: 'v-cv',
+    description:
+      'Dịch vụ cho phép bạn tạo những bản CV online hoàn toàn miễn phí' +
+      ' theo như ý thích của mình dựa trên nhiều mẫu cv đẹp, hiện đại và đảm bảo tính riêng tư.' +
+      ' Viblo CV hoàn toàn không bán, hay chia sẻ dữ liệu người dùng với các công ty tuyển dụng.',
+    logo: require('../assets/cv-logo.svg'),
+    link: 'https://cv.viblo.asia',
+    images: [require('../assets/cv-image.png')]
+  }
+]
