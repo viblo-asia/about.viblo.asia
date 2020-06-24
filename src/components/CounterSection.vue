d5b9b7bc13aa239ae834339ced2b3f2221c42e50
Nguyen Huu Kim <kimnh@webee.asia>
2020-06-24 08:45:19 +0700

    chore: update new pageview per month

--- src/components/CounterSection.vue
+++ src/components/CounterSection.vue
@@ -56,7 +56,7 @@ import defaultStats from '@/data/counter-items'
 
 axios.defaults.timeout = 5000
 
-const getVibloStats = () => axios.get(`${process.env.VUE_APP_VIBLO_API_URL}/api/about`).then(r => r.data)
+const getVibloStats = () => axios.get(`${process.env.VUE_APP_VIBLO_API_URL}/about`).then(r => r.data)
 const getCodeStats = () => axios.get(`${process.env.VUE_APP_CODE_API_URL}/api/about`).then(r => r.data)
 const getCTFStats = () => axios.get(`${process.env.VUE_APP_CTF_API_URL}/api/about`).then(r => r.data)
 
@@ -119,7 +119,7 @@ export default {
         },
         {
           name: 'Pageview trung bình hàng tháng',
-          count: this.viblo.pageviewsPerMonth
+          count: 1900000
         },
         {
           name: 'Bài viết mới mỗi tháng',
